d092b4c79efaba888482eed371b164b04530b857
Morteza Jamshidi <devmor.j@gmail.com>
2024-08-05 14:00:18 +0000

    create a basic db pacakge using mongodb driver

---
+++ db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"os"
+	"sync"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+const (
+	dbUri = "mongodb://localhost:27017"
+)
+
+var (
+	clientInstance      *mongo.Client
+	clientInstanceError error
+	mongoOnce           sync.Once
+)
+
+func GetMongoClient() (*mongo.Client, error) {
+	mongoOnce.Do(func() {
+		clientOptions := options.Client().ApplyURI(dbUri).SetAuth(options.Credential{
+			Username: os.Getenv("MONGO_INITDB_ROOT_USERNAME"),
+			Password: os.Getenv("MONGO_INITDB_ROOT_PASSWORD"),
+		})
+
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			clientInstanceError = err
+		}
+
+		if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+			clientInstanceError = err
+		}
+
+		clientInstance = client
+	})
+
+	return clientInstance, clientInstanceError
+}
